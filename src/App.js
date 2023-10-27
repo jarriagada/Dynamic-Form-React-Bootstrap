9e70c23501beb51eac3e95a5e1b892dace7e107d
jarriagada <jlas1680gmail.com>
2023-10-27 15:35:53 -0300

    feat: add CardServices

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import Introduction from './Components/Introduction';
 import NavBarMenu from './Components/NavBarMenu';
-
+import Services from '../src/Components/Services/Services';
 
 function App() {
   return (
     <div className='App'>
         <NavBarMenu />
         <Introduction  />
+        <Services />
     </div>
   );
 
